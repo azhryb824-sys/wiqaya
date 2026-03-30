44ddf711dc4fa153dd39bc539989dc10a77f2b84
azhryb824-sys <azhryb824@gmail.com>
2026-03-30 12:26:10 +0300

    Update and rename 0003_visit_extinguishers_expiry_hijri.py to 0002_visit_extinguishers_expiry_hijri.py

--- visits/migrations/0002_visit_extinguishers_expiry_hijri.py
+++ visits/migrations/0002_visit_extinguishers_expiry_hijri.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("visits", "0002_initial"),
+        ("visits", "0001_initial"),
     ]
 
     operations = [
