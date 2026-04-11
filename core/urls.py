9690eb299c4efb29ad48979047262ca97dc99adf
azhryb824-sys <azhryb824@gmail.com>
2026-04-11 13:03:38 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -21,4 +22,35 @@ urlpatterns = [
     path("users/create/", views.create_user_view, name="create_user"),
     path("users/<int:user_id>/", views.user_detail_view, name="user_detail"),
     path("users/<int:user_id>/delete/", views.delete_user_view, name="delete_user"),
+
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="core/password_reset.html",
+            email_template_name="core/password_reset_email.html",
+            subject_template_name="core/password_reset_subject.txt",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="core/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="core/password_reset_confirm.html",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="core/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
 ]
