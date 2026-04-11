5e47a478a54f3e23e366007085d8805f2fbc0a5b
azhryb824-sys <azhryb824@gmail.com>
2026-04-11 13:21:18 +0300

    Update urls.py

--- wiqaya/urls.py
+++ wiqaya/urls.py
@@ -5,7 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Core
     path("", include("core.urls")),
+
+    # Apps (مهم جداً للـ namespace)
+    path("contracts/", include(("contracts.urls", "contracts"), namespace="contracts")),
+    path("certificates/", include(("certificates.urls", "certificates"), namespace="certificates")),
+    path("visits/", include(("visits.urls", "visits"), namespace="visits")),
+    path("quotations/", include(("quotations.urls", "quotations"), namespace="quotations")),
 ]
 
 if settings.DEBUG:
